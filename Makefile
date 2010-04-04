7cd7ab3f8d77656dd8479a2645cb6077ee63d4c0
Paulo Casaes <irmaosaturno@gmail.co>
2010-04-03 21:48:23 -0300

    Changed from single precision to double precision while processing (no dithering is done when converting back to single precision).
    No longer fixed at 44.1KHz, now uses the patch'es current sampling rate (no tests done with resampling).
    
    Changed MUUG_TILDE_TABLE_SIZE preprocessor directive so it can be defined in the makefile. Default value is 512. ex: -DMUUG_TILDE_TABLE_SIZE=1024
    
    Fixed bug on tanh table. Table size should be MUUG_TILDE_TABLE_SIZE+1.
    
    Calculation error on tanh result caused when calculating index t (from the sample x). Used truncation. Changed it to nearest integer (rint) but added an option to use linear interpolation. Use preprocessor directive MUUG_INTERPOLATE, default is 0 (nearest int). ex: -DMUUG_INTERPOLATE=1
    
    Added a simple kick drum that uses muug~ as an example.

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
-muug~.pd_linux: muug~.c
-	gcc -O3 -std=c99 -Wall -pedantic -shared -o muug~.pd_linux muug~.c
+CFLAGS = -std=c99 -DPD -O3 -Wall -W -Wshadow -Wstrict-prototypes -Wno-unused -Wno-parentheses -Wno-switch -DMUUG_INTERPOLATE=1 -DMUUG_TILDE_TABLE_SIZE=512
+
+UNIVERSAL=-arch i386 -arch ppc
+DARWINCFLAGS = $(CFLAGS) -DDARWIN $(UNIVERSAL)
+DARWIN_LIBS=$(UNIVERSAL)
+
+linux: muug~.c
+	gcc $(CFLAGS) -o muug~.o -c muug~.c
+	ld -export_dynamics -shared -o muug~.pd_linux muug~.o
+	strip --strip-unneeded muug~.pd_linux
+
+darwin: muug~.c
+	  cc  -std=c99 $(DARWINCFLAGS) -pedantic -o muug~.o -c muug~.c
+		cc -bundle -undefined suppress -flat_namespace $(DARWIN_LIBS) -o muug~.pd_darwin muug~.o
+
+clean:
+	rm *.o
+	rm muug~.pd*
--- muug~.c
+++ muug~.c
@@ -1,31 +1,95 @@
+/**
+
+Copyright (C) 2006,2007,2008 Claude Heiland-Allen
+Copyright (C) 2010 Paulo Casaes
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+-- 
+http://http://gitorious.org/~saturno
+mailto:irmaosaturno@gmail.com
+
+http://claudiusmaximus.goto10.org
+mailto:claudiusmaximus@goto10.org
+
+
+*/
+
 #include "m_pd.h"
 #include <math.h>
+
+#define muug_t double   //change to float to lower processing to single precision
 
 struct muug_tilde {
   t_object pd;
   t_float dummy;
-  t_sample sr;
-  t_sample v;
-  t_sample ya1;
-  t_sample wa1;
-  t_sample yb1;
-  t_sample wb1;
-  t_sample yc1;
-  t_sample wc1;
-  t_sample yd1;
+  muug_t v;
+  muug_t ya1;
+  muug_t wa1;
+  muug_t yb1;
+  muug_t wb1;
+  muug_t yc1;
+  muug_t wc1;
+  muug_t yd1;
 } t_sigrpole;
 
 static t_class *muug_tilde_class;
 
+#define MUUG_PI 3.14159265358979323846
 #define MUUG_TILDE_TABLE_RANGE 4
-#define MUUG_TILDE_TABLE_SIZE 512
-static t_sample muug_tilde_tanh[MUUG_TILDE_TABLE_SIZE];
+#ifdef MUUG_TILDE_TABLE_SIZE
+#else
+#define MUUG_TILDE_TABLE_SIZE 512 //262144=2^18
+#endif
 
-static inline t_sample ttanh(t_sample x) {
-  int t = MUUG_TILDE_TABLE_SIZE * (x + MUUG_TILDE_TABLE_RANGE) / (MUUG_TILDE_TABLE_RANGE * 2);
+
+#ifdef MUUG_INTERPOLATE
+#else
+#define MUUG_INTERPOLATE 0
+#endif
+
+
+
+static muug_t muug_tilde_tanh[MUUG_TILDE_TABLE_SIZE+1];
+
+static inline muug_t ttanh(t_sample x) {
+#if MUUG_INTERPOLATE == 0
+  int t = rint(MUUG_TILDE_TABLE_SIZE * (x + MUUG_TILDE_TABLE_RANGE) / (MUUG_TILDE_TABLE_RANGE * 2));
   if (t < 0) t = 0;
-  if (t >= MUUG_TILDE_TABLE_SIZE) t = MUUG_TILDE_TABLE_SIZE - 1;
+  if (t > MUUG_TILDE_TABLE_SIZE) { 
+		t = MUUG_TILDE_TABLE_SIZE;
+	}
   return muug_tilde_tanh[t];
+#else
+	t_sample pos = MUUG_TILDE_TABLE_SIZE * (x + MUUG_TILDE_TABLE_RANGE) / (MUUG_TILDE_TABLE_RANGE * 2);
+  int t = ceil(pos);
+  int u = floor(pos);
+  if (t < 0) t = 0;
+  if (t > MUUG_TILDE_TABLE_SIZE) { 
+		t = MUUG_TILDE_TABLE_SIZE;
+	}
+  if (u < 0) u = 0;
+  if (u > MUUG_TILDE_TABLE_SIZE) { 
+		u = MUUG_TILDE_TABLE_SIZE;
+	}
+	if(t != u) {
+		return muug_tilde_tanh[u] + (pos -u) * ((muug_tilde_tanh[t]-muug_tilde_tanh[u])/(t-u));
+	}
+  return muug_tilde_tanh[t];
+#endif
 }
 
 #define ttanh tanhf
@@ -35,7 +99,6 @@ static void *muug_tilde_new(void) {
   inlet_new(&muug->pd, &muug->pd.ob_pd, &s_signal, &s_signal);
   inlet_new(&muug->pd, &muug->pd.ob_pd, &s_signal, &s_signal);
   outlet_new(&muug->pd, &s_signal);
-  muug->sr  = 44100; // FIXME
   muug->v   = 2;
   muug->ya1 = 0;
   muug->wa1 = 0;
@@ -44,37 +107,40 @@ static void *muug_tilde_new(void) {
   muug->yc1 = 0;
   muug->wc1 = 0;
   muug->yd1 = 0;
+	
   return muug;
 }
 
 static t_int *muug_tilde_perform(t_int *w) {
-  t_sample *in_audio      = (t_sample *)         (w[1]);
+  t_signal *inputsig      = (t_signal *)         (w[1]);
+  t_sample *in_audio      = inputsig->s_vec;
   t_sample *in_frequency  = (t_sample *)         (w[2]);
   t_sample *in_resonance  = (t_sample *)         (w[3]);
   t_sample *out_audio     = (t_sample *)         (w[4]);
   struct muug_tilde *muug = (struct muug_tilde *)(w[5]);
   int n                   = (t_int)              (w[6]);
-  t_sample pi  = 3.141592654;
-  t_sample v   = muug->v;
-  t_sample ya1 = muug->ya1;
-  t_sample wa1 = muug->wa1;
-  t_sample yb1 = muug->yb1;
-  t_sample wb1 = muug->wb1;
-  t_sample yc1 = muug->yc1;
-  t_sample wc1 = muug->wc1;
-  t_sample yd1 = muug->yd1;
+  
+
+  muug_t v   = muug->v;
+  muug_t ya1 = muug->ya1;
+  muug_t wa1 = muug->wa1;
+  muug_t yb1 = muug->yb1;
+  muug_t wb1 = muug->wb1;
+  muug_t yc1 = muug->yc1;
+  muug_t wc1 = muug->wc1;
+  muug_t yd1 = muug->yd1;
   for (int i = 0; i < n; ++i) {
     t_sample x = *in_audio++;
     t_sample f = *in_frequency++;
     t_sample r = *in_resonance++;
-    t_sample g = 1 - expf(-2 * pi * f / muug->sr);
-    t_sample ya = ya1 + v * g * ttanh((x - 4 * r * yd1) / v - wa1);
-    t_sample wa = ttanh(ya / v);
-    t_sample yb = yb1 + v * g * (wa - wb1);
-    t_sample wb = ttanh(yb / v);
-    t_sample yc = yc1 + v * g * (wb - wc1);
-    t_sample wc = ttanh(yc / v);
-    t_sample yd = yd1 + v * g * (wc - ttanh(yd1 / v));
+    muug_t g = 1 - expf(-2 * MUUG_PI * f /inputsig->s_sr);
+    muug_t ya = ya1 + v * g * ttanh((x - 4 * r * yd1) / v - wa1);
+    muug_t wa = ttanh(ya / v);
+    muug_t yb = yb1 + v * g * (wa - wb1);
+    muug_t wb = ttanh(yb / v);
+    muug_t yc = yc1 + v * g * (wb - wc1);
+    muug_t wc = ttanh(yc / v);
+    muug_t yd = yd1 + v * g * (wc - ttanh(yd1 / v));
     t_sample y = yd;
     ya1 = ya;
     wa1 = wa;
@@ -96,15 +162,20 @@ static t_int *muug_tilde_perform(t_int *w) {
 }
 
 static void muug_tilde_dsp(struct muug_tilde *muug, t_signal **sp) {
-  dsp_add(muug_tilde_perform, 6, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, muug, sp[0]->s_n);
+  dsp_add(muug_tilde_perform, 6, sp[0], sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, muug, sp[0]->s_n);
 }
 
 extern void muug_tilde_setup(void) {
-  for (int t = 0; t < MUUG_TILDE_TABLE_SIZE; ++t) {
+
+	post("muug~: loading tanh table");
+  for (int t = 0; t <= MUUG_TILDE_TABLE_SIZE; ++t) {
     t_sample x = (MUUG_TILDE_TABLE_RANGE * 2) * ((t_sample) t) / MUUG_TILDE_TABLE_SIZE - MUUG_TILDE_TABLE_RANGE;
-    muug_tilde_tanh[t] = tanhf(x);
+    muug_tilde_tanh[t] = tanh(x);
   }
   muug_tilde_class = class_new(gensym("muug~"), (t_newmethod) muug_tilde_new, 0, sizeof(struct muug_tilde), 0, 0, 0);
   CLASS_MAINSIGNALIN(muug_tilde_class, struct muug_tilde, dummy);
   class_addmethod(muug_tilde_class, (t_method) muug_tilde_dsp, gensym("dsp"), 0);
+  
+
+  
 }
